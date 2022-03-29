8ccd1bca3b53fafd717e0d1e8817235c5531dfaa
Dnyanesh SK <dnyanesh451989@gmail.com>
2022-03-29 21:11:43 +0530

    React Assignment 1

---
+++ Assignment-1/code/src/Yellow.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function Yellow()
+{
+    return (
+    <div className='yellow_container'>
+        <div className='yellow'></div>
+        <div className='yellow'></div>
+    </div>
+    )
+};
+
+export default Yellow;
